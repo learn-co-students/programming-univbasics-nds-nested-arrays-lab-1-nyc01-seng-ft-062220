b76050e350807d23675e8c7e293e1e2dba5475ab
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-15 21:16:15 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,12 @@
+shelf_1 = ["Mace", "Ginger", "Marjoram"]
+shelf_2 = ["Paprika", "Fajita Mix", "Coriander"]
+shelf_3 = ["Parsley", "Sage", "Rosemary"]
+
+spice_rack = [
+  shelf_1,
+  shelf_2,
+  shelf_3,
+]
+
+spice_rack[3][0] = "Cumin"
+=> "Parsley"
